b39d30fc6d8c4b718d16ea150282a4c3286221f9
Garrett McCullough <garrett.mccullough@build.com>
2017-09-25 21:07:00 -0700

    mind map

---
+++ tea-presentation.mm
@@ -0,0 +1,31 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1506383864419" ID="ID_941088004" MODIFIED="1506397414343" TEXT="TEA presentation">
+<node CREATED="1506397414321" ID="ID_1242388930" MODIFIED="1506397422784" POSITION="right" TEXT="Can we solve this problem?">
+<node CREATED="1506384212978" ID="ID_1101178778" MODIFIED="1506386000796" TEXT="What are the types of problems technology is good at solving?">
+<arrowlink DESTINATION="ID_1101178778" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_620461716" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1101178778" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_742592367" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1101178778" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_620461716" SOURCE="ID_1101178778" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1101178778" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_742592367" SOURCE="ID_1101178778" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1506384427295" ID="ID_18205658" MODIFIED="1506397576851" TEXT="tech is good at certain types of problems">
+<node CREATED="1506385659905" ID="ID_1127336889" MODIFIED="1506385670636" TEXT="problems are well defined/understood"/>
+<node CREATED="1506385873464" ID="ID_1669847792" MODIFIED="1506385881149" TEXT="problems that are computationally easy"/>
+<node CREATED="1506396630082" ID="ID_1185217902" MODIFIED="1506396636280" TEXT="problems with enough data"/>
+</node>
+<node CREATED="1506384444658" ID="ID_682228361" MODIFIED="1506396936348" TEXT="some relatively simple problems (for humans) can&apos;t be solved or are very difficult">
+<node CREATED="1506385548832" ID="ID_1022868906" MODIFIED="1506385562063" TEXT="questions that have qualitative answer"/>
+<node CREATED="1506385636189" ID="ID_1346434086" MODIFIED="1506385642459" TEXT="computationally hard problems">
+<node CREATED="1506397264526" ID="ID_1294392481" MODIFIED="1506397271470" TEXT="estimating hard problems"/>
+</node>
+<node CREATED="1506385935584" ID="ID_1702430844" MODIFIED="1506385969471" TEXT="people problems"/>
+<node CREATED="1506397055505" ID="ID_827074413" MODIFIED="1506397063414" TEXT="problems without enough data"/>
+</node>
+</node>
+</node>
+<node CREATED="1506396509945" ID="ID_717986638" MODIFIED="1506396520446" POSITION="right" TEXT="Should we solve this problem?">
+<node CREATED="1506397142293" ID="ID_461056105" MODIFIED="1506397155378" TEXT="Is there a big enough market?"/>
+<node CREATED="1506397156706" ID="ID_1299038513" MODIFIED="1506397165842" TEXT="Is this the correct problem?"/>
+<node CREATED="1506397203671" ID="ID_96457023" MODIFIED="1506397211841" TEXT="Is it a &quot;trivial&quot; problem?"/>
+</node>
+</node>
+</map>
